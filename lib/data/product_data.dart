21f24807bba56a01cd39154c8cfa5690de83bd14
CARLOS_SANTIAGO_GARCIA_MORALES <a.21308051281214@cbtis128.edu.mx>
2024-04-25 10:01:44 +0000

    A

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:garciac/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/picania.png",
+      title: "Cortes de carne",
+      description: "Deliciosos cortes de carne de calidad premium"),
+  ProductModel(
+      img: "assets/images/chessecake.png",
+      title: "Postres",
+      description: "La más alta reposteria digna de tus antojos"),
+  ProductModel(
+      img: "assets/images/vinito.png",
+      title: "Bebidas",
+      description: "El mejor acompañamiento para ese platillo delicioso"),
+];
